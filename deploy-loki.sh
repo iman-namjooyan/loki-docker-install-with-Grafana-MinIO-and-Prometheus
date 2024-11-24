26201a66bf066e4db3a8bf5d8418dd7e0ed3f2d8
Iman Namjouyan <im.namjuyan@gmail.com>
2024-11-24 10:06:44 +0330

    Update deploy-loki.sh
    create grafana directory data

--- deploy-loki.sh
+++ deploy-loki.sh
@@ -2,4 +2,6 @@
 mkdir -p ./prometheus-data
 touch ./prometheus-data/queries.active
 chown 65534:65534 -R ./prometheus-data/
+mkdir -p ./grafana-data/provisioning
+chown -R 472:472 grafana-data/
 docker compose up -d
